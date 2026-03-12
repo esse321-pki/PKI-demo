ca96307f6506f1e5e61e624ed9326fc4f3106c2e
esse321-pki <esse321@gmail.com>
2026-03-12 10:04:33 +0100

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,13 +109,13 @@ resource "azurerm_role_assignment" "vm_key_vault_crypto_officer" {
   skip_service_principal_aad_check = true
 }
 resource "azurerm_role_assignment" "acme_private_zone_assignment" {
-  scope                 = azurerm_resource_group.main.id
+  scope                 = data.azurerm_subscription.subscription.id
   role_definition_name  = "Private DNS Zone Contributor"
   principal_id          = azurerm_linux_virtual_machine.vm.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "key_vault_certificates_officer" {
-  scope                 = azurerm_resource_group.main.id
+  scope                 = data.azurerm_subscription.subscription.id
   role_definition_name  = "Key Vault Certificates Officer"
   principal_id          = azurerm_linux_virtual_machine.vm.identity[0].principal_id
 }
