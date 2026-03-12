8b4fd0a4d81b680647ca73c64810b9cc50418754
esse321-pki <esse321@gmail.com>
2026-03-12 10:08:26 +0100

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_key_vault" "my" {
   soft_delete_retention_days      = 90
   purge_protection_enabled        = false
   sku_name                        = "standard"
-  public_network_access_enabled   = false
+  public_network_access_enabled   = true
 }
 
 
