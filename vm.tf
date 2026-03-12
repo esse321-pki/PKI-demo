230437c4487ff605d16ef7a482026e88edac70cb
esse321-pki <esse321@gmail.com>
2026-03-12 09:19:30 +0100

    import vm

---
+++ vm.tf
@@ -0,0 +1,4 @@
+import {
+  to = azurerm_linux_virtual_machine.vm
+  id = "/subscriptions/96098664-4ddf-4492-adc7-1f5b5e08ed51/resourceGroups/rg-my/providers/Microsoft.Compute/virtualMachines/vm"
+}
